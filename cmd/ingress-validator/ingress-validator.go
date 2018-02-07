8f154988797db5481e56da6d403e9d0b50bfcb49
Scott Ringwelski <sgringwe@mtu.edu>
2018-02-07 13:41:08 -0800

    Update validator to process results and send slack message

--- cmd/ingress-validator/ingress-validator.go
+++ cmd/ingress-validator/ingress-validator.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"os"
 	"crypto/tls"
+	"crypto/x509"
 	"time"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	slack "github.com/ashwanthkumar/slack-go-webhook"
 )
+
+const MINIMUM_DAYS = 45
+
+type HostResult struct {
+	Host string
+	Certs []x509.Certificate
+}
 
 func main() {
 	fmt.Println("Starting ingress validation")
@@ -20,26 +29,25 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("There are %d ingresses in the cluster\n", len(ingresses.Items))
+	
+	var results []HostResult
 
 	// https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/api/extensions/v1beta1/types.go#L568
+	checkedHosts := make(map[string]struct{})
 	for _, ingress := range ingresses.Items {
 		// https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/api/extensions/v1beta1/types.go#L601
 		for _, rule := range ingress.Spec.Rules {
-			// https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/api/extensions/v1beta1/types.go#L652
-			fmt.Printf("Found host %s\n", rule.Host)
+			if _, checked := checkedHosts[rule.Host]; checked {
+				continue
+			}
 
-			// TODO(sgringwe): How to detect what port the ingress is on? Code comments state that either port
-			// 80 or 443 are implied, but not clear how.
-			checkHost(rule.Host + ":443")
+			// https://github.com/kubernetes/kubernetes/blob/master/staging/src/k8s.io/api/extensions/v1beta1/types.go#L652
+			results = append(results, checkHost(rule.Host))
+			checkedHosts[rule.Host] = struct{}{}
 		}
 	}
-	// For each ingress, verify that it
-	// - has TLS enabled
-	// - expires > 45 days
-	// - only supports tls 1.1, 1.2
-	
-	// Produce a json report with all of the verification info and send to configured location or log
+
+	processResults(results)
 
 	fmt.Println("Finishing ingress validation")
 }
@@ -61,8 +69,13 @@ func check(err error) {
 	}
 }
 
-func checkHost(host string) (days int) {
-	conn, err := tls.Dial("tcp", host, nil)
+func checkHost(host string) (result HostResult) {
+	result = HostResult{
+		Host:  host,
+		Certs: []x509.Certificate{},
+	}
+
+	conn, err := tls.Dial("tcp", host + ":443", nil)
 	
 	if err != nil {
 		fmt.Printf("Error checking host %s: %s\n", host, err)
@@ -70,22 +83,74 @@ func checkHost(host string) (days int) {
 	}
 	defer conn.Close()
 	
-	timeNow := time.Now()
 	checkedCerts := make(map[string]struct{})
 	for _, chain := range conn.ConnectionState().VerifiedChains {
 		for _, cert := range chain {
 			if _, checked := checkedCerts[string(cert.Signature)]; checked {
 				continue
 			}
-			checkedCerts[string(cert.Signature)] = struct{}{}
 
-			// Check the expiration compared to 45 days from now
-			days := int64(cert.NotAfter.Sub(timeNow).Hours()/24)
-			fmt.Printf("Host %s expires in %d days\n", host, days)
-			if timeNow.AddDate(0, 0, 45).After(cert.NotAfter) {
-				// Send some sort of alert
-			}
+			checkedCerts[string(cert.Signature)] = struct{}{}
+			result.Certs = append(result.Certs, *cert)
 		}
+	}
+
+	return
+}
+
+// Produce a json report with all of the verification info and send to configured location or log
+func processResults(results []HostResult) {
+	for _, result := range results {
+		processResult(result)
+	}
+}
+
+// Each host has many certs in its chain, so we need to go through each one
+// TODO: Send the data as a metric somewhere like datadog? Store raw JSON somewhere? Webhooks?
+// For each ingress, verify that it
+// - has TLS enabled
+// - expires > 45 days
+// - only supports tls 1.1, 1.2
+func processResult(result HostResult) {
+	timeNow := time.Now()
+	var minimumDays int64 = 9999
+
+	for _, cert := range result.Certs {
+		days := int64(cert.NotAfter.Sub(timeNow).Hours()/24)
+
+		if days < minimumDays {
+			minimumDays = days
+		}
+	}
+
+	fmt.Printf("Host %s certificate expires in %d days\n", result.Host, minimumDays)
+
+	if minimumDays < MINIMUM_DAYS {
+		message := fmt.Sprintf("Soon expiring certificate found for host %s! Expires in %d days.", result.Host, minimumDays)
+		fmt.Println(message)
+		sendSlackMessage(result, message)
+	}
+}
+
+func sendSlackMessage(result HostResult, message string) {
+	slackWebhook := os.Getenv("SLACK_WEBHOOK")
+
+	if slackWebhook == "" {
+		fmt.Printf("Unable to send slack webhook for soon expiring certificate for host %s, webhook is missing\n", result.Host)
+		return
+	}
+
+	payload := slack.Payload {
+		Text: message,
+		Username: "kubernetes-robot",
+		Channel: "#dev-ops",
+		IconEmoji: ":lock:",
+	}
+
+	err := slack.Send(slackWebhook, "", payload)
+	if len(err) > 0 {
+		fmt.Printf("Unable to send slack webhook for expiring certificate for host %s: %s\n", result.Host, err)
+		return
 	}
 
 	return
